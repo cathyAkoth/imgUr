a882c43b77bb1d78bd223f1a53e244a41218d01d
Akoth Cathy <catherinececilia22@gmail.com>
2022-02-18 22:21:16 +0300

    Edited main

--- src/components/Main1.js
+++ src/components/Main1.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Main1.css';
+import pic from '../images/phone.png';
 
 const Main1 = () => {
   return (
@@ -8,10 +9,10 @@ const Main1 = () => {
         <h1>Community <br/> powered</h1>
         <p>Imgur is the easiest way to discover and enjoy the magic of the Internet. It’s where you’ll find the funniest, most informative and inspiring images, memes, GIFs, and visual stories served up in an endless stream of bite-sized fun. Powered by a passionate community of people from all around the world, anyone can join to share cool stuff and vote the best to the top.
 
-You’ll always find something on Imgur to make you smile and brighten your day.</p>
+        You’ll always find something on Imgur to make you smile and brighten your day.</p>
     </div>
     <div className='Right'>
-    main
+        <img src={pic} alt={'phone'} />
     </div>
     </div>
   )
