c8a1656188198126f390d77d2211755a4fac7147
Akoth Cathy <catherinececilia22@gmail.com>
2022-02-18 22:09:50 +0300

    Added main1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import Main1 from './components/Main1';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
+      <Main1 />
     </div>
   );
 }
---
+++ src/components/Main1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Main1.css';
+
+const Main1 = () => {
+  return (
+    <div className='LeftRight'>
+    <div className='Left'>
+        <h1>Community <br/> powered</h1>
+        <p>Imgur is the easiest way to discover and enjoy the magic of the Internet. It’s where you’ll find the funniest, most informative and inspiring images, memes, GIFs, and visual stories served up in an endless stream of bite-sized fun. Powered by a passionate community of people from all around the world, anyone can join to share cool stuff and vote the best to the top.
+
+You’ll always find something on Imgur to make you smile and brighten your day.</p>
+    </div>
+    <div className='Right'>
+    main
+    </div>
+    </div>
+  )
+}
+
+export default Main1
