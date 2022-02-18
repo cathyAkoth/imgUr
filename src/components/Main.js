6650b27cc829266998e9af855c8ca5f219ca39d0
Akoth Cathy <catherinececilia22@gmail.com>
2022-02-18 21:10:11 +0300

    Added main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,11 @@ import './main.css';
 
 const Main = () => {
   return (
-    <div>main</div>
+    <div className='main'>
+        <div className='contents'>
+           <p>The Magic of <br/>the Internet</p>
+        </div>
+    </div>
   )
 }
 
