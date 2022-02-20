2892ccfbe06f4197bf8f062df57bd4cb0d1b6fc3
cathyAkoth <78339848+cathyAkoth@users.noreply.github.com>
2022-02-20 23:15:56 +0300

    edited

--- src/pages/First.js
+++ src/pages/First.js
@@ -17,7 +17,7 @@ const First = () => {
         <h1>Community <br/> powered</h1>
         <p>Imgur is the easiest way to discover and enjoy the magic of the Internet. It’s where you’ll find the funniest, most informative and inspiring images, memes, GIFs, and visual stories served up in an endless stream of bite-sized fun. Powered by a passionate community of people from all around the world, anyone can join to share cool stuff and vote the best to the top.
 
-        You’ll always find something on Imgur to make you smile and brighten your day.</p>
+        You’ll always find something on Imgur to make you smile and brighten your day</p>
     </div>
     <div className='Right'>
         <img src={pic} alt={'phone'} />
